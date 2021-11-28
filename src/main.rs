40c420df8eec7587b03a557f93c4f15ae23df84e
Anders Olme <anders.olme@gmail.com>
2021-11-28 10:18:08 +0100

    fixed layout!!
    Now it dont look too bad!!

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,20 @@ fn creategui() -> app::App {
     let app = app::App::default();
     let mut wind = Window::default().with_size(640, 200).with_label("Searcher");
     // Vertical is default. You can choose horizontal using pack.set_type(PackType::Horizontal);
-    let mut main_col = Flex::default_fill().column();
-    main_col.set_pad(10);
-    let mut first_row = Flex::default().size_of_parent().row();
-    let mut but_path = Button::default().with_size(0, 40).with_label("Path");
-    let spacer1 = Frame::default();
+    let mut flex = Flex::default().size_of_parent().column();
+
+    let mut flex_r1 = Flex::default().size_of_parent().row();
+    let mut but_path = Button::default().with_size(10, 40).with_label("Path");
+    flex_r1.set_size(&mut but_path, 60);
     let mut input_path = Input::default().with_size(300, 40);
-    main_col.set_size(&spacer1,10);
-    first_row.end();
-    let mut second_row = Flex::default().size_of_parent().row();
-    let mut but_search = Button::default().with_size(0, 40).with_label("Search");
-    //let spacer2 = Frame::default();
+    flex_r1.end();
+
+    let mut flex_r2 = Flex::default().size_of_parent().row();
+    let mut but_search = Button::default().with_size(10, 40).with_label("Search");
+    flex_r2.set_size(&mut but_search, 60);
     let mut input_search = Input::default().with_size(300, 40);
-    //main_col.set_size(&spacer2, 10);
-    second_row.end();
-    main_col.end();
-    wind.resizable(&main_col);
+    flex_r2.end();
+    flex.end();
     wind.end();
     wind.show();
     app
